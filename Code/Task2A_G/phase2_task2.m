e5c8986800787415b7e0e2ea4483c36f0eccb5bc
yuhsuanchuang <flora831007@gmail.com>
2016-10-06 21:41:49 -0700

    task2 (almost done, except the part of get similarity value from task1 )
    1. extract the frame range [a, b]
    
    2. create framerange.txt and queryframerange.txt
    
    3. (incomplete)put these 2 txt file to task1 functions and get the
    similarity value
    
    4. store the similarity value and the compared video frame info to
    similaritycompare.txt
    
    5. find the k most similar video frame range and output the result as
    video (it will create k videos)

--- Code/Task2A_G/phase2_task2.m
+++ Code/Task2A_G/phase2_task2.m
@@ -3,10 +3,10 @@ function task2(v, a, b, inFile, type, k, outputPath)
 %     a = 3;
 %     b = 5;
 %     k = 3;
-    inFile = 'out_file.mvect.txt';
+%     inFile = 'out_file.mvect.txt';
     O = dlmread(inFile);
     [row column]=size(O);
-
+    delete ('framerange.txt');
     fid = fopen('framerange.txt', 'a');
     maxfilenumber = max(O(:,1));
 
@@ -21,7 +21,9 @@ function task2(v, a, b, inFile, type, k, outputPath)
                 fprintf(fid, '\n');
         end
     end
-
+    delete ('similaritycompare.txt');
+    fido = fopen('similaritycompare.txt', 'a');
+    
     for video=1:maxfilenumber
         if (video ~= v)
             qframe = O(find(O(:,1) == video), 1:column);
@@ -41,15 +43,53 @@ function task2(v, a, b, inFile, type, k, outputPath)
 
                      end
                  end
-                %fprintf(fidq, '%d\n', max(qframe(:,2)));
-                fprintf(fidq, '\n');
                 
-                %%%%%%%%%similarity
+                %get similarity -> simivalue = task1_a() 
                 if (type == 'a')
-                    
+                    %%%call task1_a input 2 files : framerange.txt , queryframerange.txt
+                    %%%store the return similarity value in (int)variable simivalue
+                    % simivalue = task1_a() <- i dont know how to call
+                elseif (type == 'b')
+                    %%%call task1_b input 2 files : framerange.txt , queryframerange.txt
+                    %%%store the return similarity value in (int)variable simivalue
+                elseif (type == 'c')
+                    %%%call task1_c input 2 files : framerange.txt , queryframerange.txt
+                    %%%store the return similarity value in (int)variable simivalue
+                elseif (type == 'd')
+                    %%%call task1_d input 2 files : framerange.txt , queryframerange.txt
+                    %%%store the return similarity value in (int)variable simivalue
+                elseif (type == 'e')
+                    %%%call task1_e input 2 files : framerange.txt , queryframerange.txt
+                    %%%store the return similarity value in (int)variable simivalue
+                elseif (type == 'f')
+                    %%%call task1_f input 2 files : framerange.txt , queryframerange.txt
+                    %%%store the return similarity value in (int)variable simivalue
+                elseif (type == 'g')
+                    %%%call task1_g input 2 files : framerange.txt , queryframerange.txt
+                    %%%store the return similarity value in (int)variable simivalue
                 end
+                fprintf(fido, '%d %d %d %d\n', simivalue, video, frame, frame+b-a-1); %fprint the compared similarity value 
 
             end
         end
     end
+    
+    % sort the similarity value and find the k most similar video
+    % frame range
+    % -> output the result as video (it will create k videos)
+    S = dlmread('similaritycompare.txt');
+    [B,I] = sort(S(:,1),'descend');
+    for kk=1:k
+        matchvideo = VideoReader(list(S(I(kk), 2)).name);
+        createvideofilename = [ num2str(kk) '_similar.avi'];
+        outputVideo = VideoWriter(createvideofilename);
+        outputVideo.FrameRate = matchvideo.FrameRate;
+        
+        for ii = S(I(kk), 3):S(I(kk), 4)
+           img = read(matchvideo, ii);
+           writeVideo(outputVideo,img)
+        end
+        close(outputVideo);
+    end
+    fclose('all');
 end
