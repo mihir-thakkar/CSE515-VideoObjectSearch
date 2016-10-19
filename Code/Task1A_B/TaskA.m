dec83055f0fa83b14a18ffca2a6a1f1fb0a5cf86
srujank1 <srujank1@users.noreply.github.com>
2016-10-19 09:33:22 -0700

    Update TaskA.m

--- Code/Task1A_B/TaskA.m
+++ Code/Task1A_B/TaskA.m
@@ -1,4 +1,4 @@
-function sim = TaskA(video_file_number, compare_video_file_number, database)
+function sim = TaskA(IndexFile ,video_file_name, compare_video_file_name, database)
 % Calculate the Euclidean distance between two videos.
-    sim = TaskAorB(video_file_number, compare_video_file_number, database, 0);
+    sim = TaskAorB(IndexFile ,video_file_name, compare_video_file_name, database, 0);
 end
