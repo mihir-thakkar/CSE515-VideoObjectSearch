194ea44f8e543c63b336956e26133b73435489bf
srujank1 <skanteti@asu.edu>
2016-10-21 12:11:39 -0700

    Update -- Mihir

--- Code/Task3A_C/pcaConvert.m
+++ Code/Task3A_C/pcaConvert.m
@@ -3,7 +3,7 @@ whole_db = dlmread(inFile);
 matrix = whole_db(:, 4:end);
 % Get the matrix of video, frame and cell indices
 vfcMatrix = whole_db(:, 1:3);
-[coeff, scores, latent] = pca(matrix, 'NumComponents', outDimen);
+[coeff, scores, latent] = pca(matrix, 'NumComponents', outDimen, 'Algorithm','eig');
 resultMatrix = horzcat(vfcMatrix, scores);
 dlmwrite(outFile, resultMatrix);
 end
