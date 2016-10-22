2fa7f4f91d5e73ca8c132ac7987ec1ba8e5b7047
srujank1 <srujank1@users.noreply.github.com>
2016-10-21 18:18:06 -0700

    Update file read

--- Code/Task1A_B/retriveDataforFile.m
+++ Code/Task1A_B/retriveDataforFile.m
@@ -5,23 +5,15 @@ function file_matrix = retriveDataforFile(file_name)
     file_number = getFileNumber(file_name); 
     
     %Get the file name
-    whole_db = dlmread('in_file.chst');
+    whole_db = dlmread('Input/in_file.chst');
     file_matrix =  whole_db(whole_db(:,1) == file_number, :);
 end
 
 function [number_file] = getFileNumber(file_name)
     
-    fileID = fopen('in_file_index.chst','r');
-    number_file = 0; 
-    while ~feof(fileID)
-        number_file = number_file +1;
-        current_file = textscan(fileID,'%s',1,'Delimiter','\n');
-        if strcmp(file_name,current_file{1})
-           fclose(fileID);  
-           return
-        end
-    end
+    fileID = fopen('Input/in_file.index','r');
+    fileIndex = textscan(fileID,'%s %d','delimiter','=','headerLines', 1);
+    fclose(fileID);
     
-    fclose(fileID);  
+    number_file = find(strcmp(fileIndex{1}(:,1), file_name)); 
 end
-
