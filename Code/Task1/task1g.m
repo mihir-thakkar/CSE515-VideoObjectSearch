e201e68c425e2d43ecc6798aeac3bf5522e2c861
yuhsuanchuang <flora831007@gmail.com>
2016-10-23 20:25:10 -0700

    update task1g-h and test results

--- Code/Task1/task1g.m
+++ Code/Task1/task1g.m
@@ -5,7 +5,7 @@ histSimWeight = 0.4609;
 siftSimWeight = 0.5157;
 motionSimWeight = 0.0233;
 histSim = task1a(videoName1, videoName2);
-siftSim = task1d(videoName1, videoName2);   
+siftSim = task1c(videoName1, videoName2);   
 motionSim = task1e(videoName1, videoName2);
 sim = histSim * histSimWeight + siftSim * siftSimWeight + motionSim * motionSimWeight;
 end
--- Code/Task1/task1h.m
+++ Code/Task1/task1h.m
@@ -4,7 +4,7 @@ histSimWeight = 0.4609;
 siftSimWeight = 0.5157;
 motionSimWeight = 0.0233;
 histSim = task1b(videoName1, videoName2);
-siftSim = task1c(videoName1, videoName2);   
+siftSim = task1d(videoName1, videoName2);   
 motionSim = task1f(videoName1, videoName2);
 sim = histSim * histSimWeight + siftSim * siftSimWeight + motionSim * motionSimWeight;
 end
