0b34ec57d550871c40f735963d97984b1ac3c1aa
Yiqian Zhang <zyq32516@gmail.com>
2016-11-18 19:19:05 -0700

    Phase3_Task4_pgrk_personalized

--- Phase3/Code/Phase3_Task4_pgrank_personalized.py
+++ Phase3/Code/Phase3_Task4_pgrank_personalized.py
@@ -4,11 +4,13 @@ import operator
 import csv
 
 INPUT_FILE = '../Output/output_t2_d_5.gspc';
-OUTPUT_FILE = '../Output/pageranks.csv';
+OUTPUT_FILE = '../Output/pageranks_personalized.csv';
 
 node_dict = {}
-input_frames = ("1_44", "1_46", "1_47")     #how to input the query frame?
-#print input_frames[0], input_frames[1], input_frames[2]
+array = raw_input("please input three query frames: ");     #input format:1_44 1_46 1_47
+m = int(input("please input the most significant m frames (relative to the input frames): "))
+input_frames = array.split(' ');
+print(input_frames);
 
 def add_edges_from_line(D, p_str):
     str = re.sub('\{|\}|\(|\)|\n|,', ' ', p_str);
@@ -18,32 +20,33 @@ def add_edges_from_line(D, p_str):
 #    print arr;
     k = len(arr) / 5;
     for i in range(k):
-#        print i
         si = i * 5;
         node1 = arr[si] + '_' +arr[si + 1];
         if node1 == input_frames[0] or node1 == input_frames[1] or node1 == input_frames[2]:
-            node_dict[node1] = 1;   #How to determine the threshold of the query node?
+            node_dict[node1] = float(1)/3;
         else:
-            node_dict[node1]= 0.1;
+            node_dict[node1] = 0;
 #        print node1
         node2 = arr[si + 2] + '_' + arr[si + 3];
-        if node2 == input_frames[0] or node2 == input_frames[1] or node2 == input_frames[2]:
-            node_dict[node2] = 1;
-        else:
-            node_dict[node2] = 0.1;
-#        print node2
+#            if node2 == input_frames[0] or node2 == input_frames[1] or node2 == input_frames[2]:
+#                node_dict[node2] = float(1)/3;
+#            else:
+#                node_dict[node2] = 0;
         D.add_weighted_edges_from([(node1, node2, float(arr[si + 4]))]);
 
-
 D = nx.DiGraph();
-#node_dict["6_17"]= 0.0001;
 with open(INPUT_FILE, 'r') as ins:
     for line in ins:
         add_edges_from_line(D, line);
+
 prs = nx.pagerank(D, personalization=node_dict);
-#sorted_prs = sorted(prs.items(), key=operator.itemgetter(1), reverse=True);
-#print sorted_prs;
-print prs
+sorted_prs = sorted(prs.items(), key=operator.itemgetter(1), reverse=True);
+print sorted_prs;
+#print prs
 w = csv.writer(open(OUTPUT_FILE, 'w'));
-#for key, val in sorted_prs:
-#    w.writerow([key, val]);
+k = 1;
+for key, val in sorted_prs:
+    w.writerow([key, val]);
+    k = k + 1;
+    if k > m:
+        break;
