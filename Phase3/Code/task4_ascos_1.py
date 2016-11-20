0f397ee1bb429dcda86f4909e1f9c077c47c73b2
lei <lei.guo.4@asu.edu>
2016-11-19 23:57:12 -0700

    add output frames

--- Phase3/Code/task4_ascos_1.py
+++ Phase3/Code/task4_ascos_1.py
@@ -8,13 +8,14 @@ import math
 import re
 import operator
 import csv
+import utils
 
 INPUT_FILE = '../Output/output_t2_d_5.gspc';
-OUTPUT_FILE = '../Output/output_ranks_ascos.csv';
+OUTPUT_FILE = '../Output/output_t4_ascos_1.csv';
+global first_intput, second_intput, third_intput, alpha;
 
 
 def ascos(G, c=0.9, max_iter=100, remove_neighbors=False, remove_self=False, dump_process=False):
-
     if type(G) == nx.MultiGraph or type(G) == nx.MultiDiGraph:
         raise Exception("ascos() not defined for graphs with multiedges.")
 
@@ -22,12 +23,12 @@ def ascos(G, c=0.9, max_iter=100, remove_neighbors=False, remove_self=False, dum
     node_id_lookup_tbl = {}
     for i, n in enumerate(node_ids):
         node_id_lookup_tbl[n] = i
-    # Use predecessors
+    # Use successors
     nb_ids = [G.successors(n) for n in node_ids]
     nbs = []
     for nb_id in nb_ids:
         nbs.append([node_id_lookup_tbl[n] for n in nb_id])
-    del (node_id_lookup_tbl)
+    # del (node_id_lookup_tbl)
 
     n = G.number_of_nodes()
     sim = numpy.eye(n)
@@ -40,7 +41,7 @@ def ascos(G, c=0.9, max_iter=100, remove_neighbors=False, remove_self=False, dum
         for i in range(n):
             if dump_process:
                 print iter_ctr, ':', i, '/', n
-            # Use in degree
+            # Use out degree
             w_i = G.out_degree(weight='weight')[node_ids[i]]
             for j in range(n):
                 if i == j:
@@ -52,6 +53,18 @@ def ascos(G, c=0.9, max_iter=100, remove_neighbors=False, remove_self=False, dum
                     s_ij += float(w_ki) * (1 - math.exp(-w_ki)) * sim_old[n_i, j]
 
                 sim[i, j] = c * s_ij / w_i if w_i > 0 else 0
+                # if (node_ids[i] == first_intput or second_intput or third_intput) and sim[i, j] != 0:
+                #     sim[i, j] = alpha * sim[i, j] + (1 - alpha) * 0.3333333333;
+        for i in range(n):
+            j = node_id_lookup_tbl[first_intput];
+            if sim[i, j] != 0:
+                sim[i, j] = alpha * sim[i, j] + (1 - alpha) * 0.33;
+            j = node_id_lookup_tbl[second_intput];
+            if sim[i, j] != 0:
+                sim[i, j] = alpha * sim[i, j] + (1 - alpha) * 0.33;
+            j = node_id_lookup_tbl[third_intput];
+            if sim[i, j] != 0:
+                sim[i, j] = alpha * sim[i, j] + (1 - alpha) * 0.33;
 
     if remove_self:
         for i in range(n):
@@ -85,24 +98,46 @@ def add_edges_from_line(D, p_str):
         node2 = arr[si + 2] + '_' + arr[si + 3];
         D.add_weighted_edges_from([(node1, node2, float(arr[si + 4]))]);
 
+# Function : Main
+# Description: Run the main program
+if __name__ == '__main__':
+    # Take k as an input
+    m = int(input("Enter m, for the m most significant frames (relative to the input frames):"))
+    first_intput = input("Enter first input frame:")
+    second_intput = input("Enter second input frame:")
+    third_intput = input("Enter 5 input frame:")
+    alpha = 0.85;
 
-D = nx.DiGraph();
-with open(INPUT_FILE, 'r') as ins:
-    for line in ins:
-        add_edges_from_line(D, line);
-node_ids, sim = ascos(D);
-# print node_ids;
-# node ranks
-prs = {}
-length = len(node_ids);
-for j in range(length):
-    simsum = 0;
-    for i in range(length):
-        if i != j:
-            simsum += sim[i, j];
-    w = simsum / (length - 1);
-    prs.update({node_ids[j]: w});
-sorted_prs = sorted(prs.items(), key=operator.itemgetter(1), reverse=True);
-w = csv.writer(open(OUTPUT_FILE, 'w'));
-for key, val in sorted_prs:
-    w.writerow([key, val]);
+    D = nx.DiGraph();
+    with open(INPUT_FILE, 'r') as ins:
+        for line in ins:
+            add_edges_from_line(D, line);
+    node_ids, sim = ascos(D);
+    # print node_ids;
+    # node ranks
+    prs = {}
+    length = len(node_ids);
+    for j in range(length):
+        simsum = 0;
+        for i in range(length):
+            if i != j:
+                simsum += sim[i, j];
+        w = simsum / (length - 1);
+        prs.update({node_ids[j]: w});
+    sorted_prs = sorted(prs.items(), key=operator.itemgetter(1), reverse=True);
+
+    # output frames
+    utils.output_a_frame(first_intput, 'input')
+    utils.output_a_frame(second_intput, 'input')
+    utils.output_a_frame(third_intput, 'input')
+    counter = 1
+    for key, val in sorted_prs:
+        w.writerow([key, val])
+        # output frames
+        if counter <= m:
+            counter += 1
+            utils.output_a_frame(key, 'output', counter);
+
+    w = csv.writer(open(OUTPUT_FILE, 'w'));
+    for key, val in sorted_prs:
+        w.writerow([key, val]);
---
+++ Phase3/Code/utils.py
@@ -0,0 +1,32 @@
+import imageio
+import numpy as np
+import os
+
+INPUT_VIDEO_PATH = '../Input/Videos/'
+INPUT_INDEX = '../Input/in_file.index'
+OUTPUT_PATH = '../Output/Frames/'
+
+def output_frame(video_index, frame_index, output_name):
+
+    #Creating video name to video num index and reverse index
+    fileIndex = np.genfromtxt(INPUT_INDEX, delimiter="=", dtype=None, skip_header=1)
+    fileIndex = dict(fileIndex)
+    revIndex = {v: k for k, v in fileIndex.iteritems()}
+
+    # Creating output path if not exist
+    if not os.path.isdir(OUTPUT_PATH):
+        os.mkdir(OUTPUT_PATH)
+
+    video_name = revIndex[int(video_index)];
+    objectFile = imageio.get_reader(INPUT_VIDEO_PATH + video_name, 'ffmpeg')
+    oimage = objectFile.get_data(frame_index)
+    imageio.imwrite(OUTPUT_PATH + output_name, oimage)
+
+
+def output_a_frame(vi_fi, prefix, rank=None):
+    a = str(vi_fi).split('_');
+    if rank is not None:
+        output_name = prefix + '_' + vi_fi + '_rank' + rank + '.png';
+    else:
+        output_name = prefix + '_' + vi_fi + '.png';
+    output_frame(a[0], a[1], output_name);
