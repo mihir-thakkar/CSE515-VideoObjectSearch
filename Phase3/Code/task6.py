7415287decb206072f959f5cd7fb318f261453da
Mihir Thakkar <mihir9293@gmail.com>
2016-11-27 16:07:54 -0700

    visualize frame function- not tested code

--- Phase3/Code/task6.py
+++ Phase3/Code/task6.py
@@ -4,7 +4,7 @@ from scipy.spatial.distance import cdist
 import imageio
 
 # SIFT desc Information
-global START_COL, VIDEO_NUM_COL, FRAME_NUM_COL, CELL_NUM_COL, SIFT_DES_START
+global START_COL, VIDEO_NUM_COL, FRAME_NUM_COL, CELL_NUM_COL, SIFT_DES_START, LSH_HASH_COL, LSH_BUCKET_COL, DATA_INDEX_COL, X_COL, Y_COL
 START_COL = 0
 VIDEO_NUM_COL = 3
 FRAME_NUM_COL = 4
@@ -16,13 +16,19 @@ X_COL = 6
 Y_COL = 7
 
 
-# SIFT I/O Information
-global INPUT_FILE
+#SIFT I/O Information
+global INPUT_FILE, INPUT_DB_INDEX, INPUT_VIDEO_PREFIX, OUTPUT_FRAMES_PREFIX,INPUT_PREFIX
 INPUT_FILE = "../Output/filename_d.lsh"
+INPUT_PREFIX = "../../Input/"
+INPUT_DB_INDEX = "in_file.index"
+INPUT_VIDEO_PREFIX = "../../Input/Videos/"
+OUTPUT_FRAMES_PREFIX = "../../Output/n_frames/"
 
 # Function : preProcessing
 # Description: This function loads the database and clears the input file
 def preProcessing():
+
+    global fileIndex, revIndex, database
 
     # Clear the file
     # printerFile = open("../Output/" + "output_t2_d_" + str(K) + ".gspc", "wb")
@@ -36,6 +42,13 @@ def preProcessing():
     databasePD = pd.read_csv(INPUT_FILE,header=None,names=['b','h','i','v','f','c','x','y'])
 
     print 'Database loaded......'
+
+    # Creating file index
+    fileIndex = np.genfromtxt(INPUT_PREFIX + INPUT_DB_INDEX, delimiter="=", dtype=None, skip_header=1)
+    fileIndex = dict(fileIndex)
+
+    print "stop"
+
 
 def search(N,V,F,X1,Y1,X2,Y2):
     samples = databasePD[(databasePD.v == V) & (databasePD.f == F) & (databasePD.x>=X1) & (databasePD.x<=X2) & (databasePD.y>=Y1) & (databasePD.y<=Y2)]
@@ -66,12 +79,19 @@ def search(N,V,F,X1,Y1,X2,Y2):
 
     for i in range(0,N):
         print 'Video No: %s Frame No: %s' % (str(frameSim[i:0]), str(frameSim[i:1]))
+        #outputFrame(frameSim[i:0],frameSim[i:1])
     print 'done'
+
+def outputFrame(videoIndex, frameIndex):
+
+    Frame = imageio.get_reader(INPUT_VIDEO_PREFIX + fileIn[int(videoIndex)], 'ffmpeg')
+    writer = imageio.get_writer(OUTPUT_FRAMES_PREFIX + str(videoIndex) + str(frameIndex) + '.jpg')
+    writer.append_data(Frame)
+    writer.close()
 
 # Function : Main
 # Description: Run the main program
 if __name__ == '__main__':
-
 
     # Pre-processing
     preProcessing()
@@ -80,4 +100,3 @@ if __name__ == '__main__':
         N = int(input("Enter n, for number of frames: "))
         (V,F,X1,Y1,X2,Y2) = map(int, raw_input("Enter Video, Frame No and 2 points seperated by space (V F X1 Y1 X2 Y2): ").split())
         search(N, V, F, X1, Y1, X2, Y2)
-
