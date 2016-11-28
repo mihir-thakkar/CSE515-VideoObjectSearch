420c49d4ddc8cdb359c65c417d20e665b9f7081b
srujank1 <skanteti@asu.edu>
2016-11-27 18:35:22 -0700

    Task 6 mem ----- updated

--- Phase3/Code/task6.py
+++ Phase3/Code/task6.py
@@ -19,50 +19,57 @@ Y_COL = 7
 #SIFT I/O Information
 global INPUT_FILE, INPUT_DB_INDEX, INPUT_VIDEO_PREFIX, OUTPUT_FRAMES_PREFIX,INPUT_PREFIX
 INPUT_FILE = "../Output/filename_d.lsh"
-INPUT_PREFIX = "../../Input/"
+INPUT_PREFIX = "../Input/"
 INPUT_DB_INDEX = "in_file.index"
-INPUT_VIDEO_PREFIX = "../../Input/Videos/"
-OUTPUT_FRAMES_PREFIX = "../../Output/n_frames/"
+INPUT_VIDEO_PREFIX = "../Input/Videos/"
+OUTPUT_FRAMES_PREFIX = "../Output/n_frames/"
 
 # Function : preProcessing
 # Description: This function loads the database and clears the input file
-def preProcessing():
+def preProcessing(fileName):
 
     global fileIndex, revIndex, database
-
-    # Clear the file
-    # printerFile = open("../Output/" + "output_t2_d_" + str(K) + ".gspc", "wb")
-    # printerFile.close()
 
     # Load the database
     print 'Loading database......'
 
     global database, databasePD
-    database = np.loadtxt("../Input/in_file.sift", delimiter=",")
+    database = np.loadtxt(INPUT_PREFIX + fileName + ".lsh", delimiter=",")
     databasePD = pd.read_csv(INPUT_FILE,header=None,names=['b','h','i','v','f','c','x','y'])
-
+    print database.nbytes
+    print databasePD.m
     print 'Database loaded......'
 
     # Creating file index
     fileIndex = np.genfromtxt(INPUT_PREFIX + INPUT_DB_INDEX, delimiter="=", dtype=None, skip_header=1)
     fileIndex = dict(fileIndex)
-
+    revIndex = {v: k for k, v in fileIndex.iteritems()}
     print "stop"
 
 
 def search(N,V,F,X1,Y1,X2,Y2):
+    totalDataAccessSize = 0
+    totalIndexAccessSize = 0
     samples = databasePD[(databasePD.v == V) & (databasePD.f == F) & (databasePD.x>=X1) & (databasePD.x<=X2) & (databasePD.y>=Y1) & (databasePD.y<=Y2)]
+    totalIndexAccessSize += samples.memory_usage().sum()
     selIndexes = samples['i']
     selIndexes = selIndexes.drop_duplicates()
     sampleArray = database[selIndexes,:]
+    totalDataAccessSize += sampleArray.nbytes
     samples = samples[['b', 'h']]
     result = pd.merge(samples, databasePD, how='inner', on=['b', 'h'])
     result = result[(result.v != V)]
     overallSiftVectors = result.shape[0]
+    print 'Overall Sift Vectors Considered from all buckets: %s' % str(overallSiftVectors)
     result = result.drop_duplicates('i')
+    totalIndexAccessSize += result.memory_usage().sum()
     result = result.sort_values('i')
     uniqueSiftVectors = result.shape[0]
+    print 'Unique Sift Vectors Considered from all buckets: %s' % str(uniqueSiftVectors)
     prunedData = database[result['i'],:]
+    totalDataAccessSize += prunedData.nbytes
+    print 'Index Access Data Size: %s' % str(totalIndexAccessSize)
+    print 'Original Database access Size: %s' % str(totalDataAccessSize)
 
     videoNos = np.transpose(np.unique(prunedData[:, 0]))
     frameSim = np.array([]).reshape(0, 3)
@@ -78,23 +85,24 @@ def search(N,V,F,X1,Y1,X2,Y2):
     frameSim = frameSim[np.argsort(frameSim[:, 2])]
 
     for i in range(0,N):
-        print 'Video No: %s Frame No: %s' % (str(frameSim[i:0]), str(frameSim[i:1]))
-        #outputFrame(frameSim[i:0],frameSim[i:1])
+        print 'Video No: %s Frame No: %s' % (str(frameSim[i,0]), str(frameSim[i,1]))
+        outputFrame(frameSim[i,0],frameSim[i,1])
     print 'done'
 
 def outputFrame(videoIndex, frameIndex):
 
-    Frame = imageio.get_reader(INPUT_VIDEO_PREFIX + fileIn[int(videoIndex)], 'ffmpeg')
-    writer = imageio.get_writer(OUTPUT_FRAMES_PREFIX + str(videoIndex) + str(frameIndex) + '.jpg')
-    writer.append_data(Frame)
-    writer.close()
+    video = imageio.get_reader(INPUT_VIDEO_PREFIX + revIndex[int(videoIndex)], 'ffmpeg')
+    frame = video.get_data(int(frameIndex))
+    imageio.imwrite(OUTPUT_FRAMES_PREFIX + "v"+ str(videoIndex) + "_f"+str(frameIndex) + '.jpg', frame)
 
 # Function : Main
 # Description: Run the main program
 if __name__ == '__main__':
-
+    INPUT_FILE = raw_input("Enter the input file name(File should exist in Input directory): ")
+    fileName = INPUT_FILE.split(".")[0]
+    INPUT_FILE = INPUT_PREFIX + INPUT_FILE
     # Pre-processing
-    preProcessing()
+    preProcessing(fileName)
     while 1:
         # Take k as an input
         N = int(input("Enter n, for number of frames: "))
